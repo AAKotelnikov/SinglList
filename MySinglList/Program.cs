d2a7073f0f52b50c74862e370782d343ba1b2ad9
Kotelnikov <sewi.93@mail.ru>
2013-12-28 14:12:36 +0600

    KotelnikovCommit

---
+++ MySinglList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySinglList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglList MyList = new SinglList(3, new SinglList(5, new SinglList(9)));
+            Console.WriteLine("Список:");
+            MyList.ShowList();
+
+            Console.Write("Значение нового элемента для вставки: "); int NewValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Позиция нового элемента для вставки: "); int Pos = Convert.ToInt32(Console.ReadLine());
+            MyList.PastIn(NewValue, Pos);
+
+            Console.WriteLine("Список:");
+            MyList.ShowList();
+
+            Console.Write("Позиция элемента для удаления: "); Pos = Convert.ToInt32(Console.ReadLine());
+            MyList.Delete(Pos);
+
+            Console.WriteLine("Список:");
+            MyList.ShowList();
+
+            Console.Write("Позиция элемента для получения: "); Pos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MyList.GetValue(Pos));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySinglList/SinglList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySinglList
+{
+    class SinglList
+    {
+        private int itemvalue;
+        private SinglList nextitem;
+
+        public int ItemValue
+        {
+            get { return itemvalue; }
+            set { itemvalue = value; }
+        }
+
+        public SinglList NextItem
+        {
+            get { return nextitem; }
+            set { nextitem = value; }
+        }
+
+        public SinglList(int NewValue)
+        {
+            ItemValue = NewValue;
+            NextItem = null;
+        }
+
+        public SinglList(int NewValue, SinglList NewNextItem)
+        {
+            ItemValue = NewValue;
+            NextItem = NewNextItem;
+        }
+
+        public void ShowList()
+        {
+            if (NextItem == null)
+            {
+                Console.WriteLine(ItemValue);
+            }
+            else
+            {
+                Console.Write(ItemValue+" ");
+                NextItem.ShowList();
+            }
+        }
+
+        public int GetValue(int position)
+        {
+            try
+            {
+                if (position == 1)
+                {
+                    return ItemValue;
+                }
+                else
+                {
+                    return NextItem.GetValue(position - 1);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверное значение позиции. Возвращен 0");
+                return 0;
+            }
+        }
+
+        public void PastIn(int NewValue, int position)
+        {
+            try
+            {
+                if (position == 1)
+                {
+                    SinglList ThisItem = new SinglList(ItemValue, NextItem);
+                    this.ItemValue = NewValue;
+                    this.NextItem = ThisItem;
+                }
+                else
+                {
+                    if (NextItem != null)
+                    {
+                        NextItem.PastIn(NewValue, position - 1);
+                    }
+                    else
+                    {
+                        NextItem = new SinglList(NewValue);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверное значение позиции.");
+            }
+        }
+
+        public void Delete(int position)
+        {
+            try
+            {
+                if (position == 1)
+                {
+                    this.ItemValue = NextItem.ItemValue;
+                    this.NextItem = NextItem.NextItem;
+                }
+                else
+                {
+                    if ((position - 1 == 1) & (NextItem.NextItem == null))
+                    {
+                        NextItem = null;
+                    }
+                    else
+                    {
+                        NextItem.Delete(position - 1);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверное значение позиции.");
+            }
+        }
+    }
+}
